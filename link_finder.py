496c219448ff0f7d8fac8dcc643f47658d424adb
mehdi dridi <mehdignu@users.noreply.github.com>
2018-07-28 02:48:06 +0200

    add spider

--- link_finder.py
+++ link_finder.py
@@ -1,14 +1,28 @@
 # find all the links inside a HTML page
 from HTMLParser import HTMLParser
-from urlparse import urlparse
+from urlparse import *
+
 
 class LinkFinder(HTMLParser):
 
-    def __init__(self):
+    def __init__(self, base_url, page_url):
         HTMLParser.__init__(self)
+        self.base_url = base_url
+        self.page_url = page_url
+        self.links = set()
 
     def handle_starttag(self, tag, attrs):
-        print "Encountered a start tag:", tag
+        if tag == 'a':
+            for (attribute, value) in attrs:
+                if attribute == 'href':
+                    url = urljoin(self.base_url, value)
+                    self.links.add(url)
+
+
+    def page_links(self):
+        return self.links
+
+
 
 
     def error(self, message):
